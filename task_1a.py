7baa59a8ed6182150aeed36b034e9b907d64a876
ihorborys <ihorborys@gmail.com>
2024-10-24 15:12:54 +0300

    Edited task_1a, task_1b

--- task_1a.py
+++ task_1a.py
@@ -15,4 +15,4 @@ print(find_coins_greedy(113))  # {50: 2, 10: 1, 2: 1, 1: 1}
 # print(find_coins_greedy(11300000))  # {50: 226000}
 
 greedy_time = timeit.timeit('find_coins_greedy(113)', globals=globals(), number=1000)
-print("Час виконання жадібного алгоритму  на 1000 прогонів:", greedy_time, "секунд")
+print("Час виконання жадібного алгоритму на 1000 прогонів:", greedy_time, "секунд")
